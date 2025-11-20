c849ba155fefac2e17739781c658a8792be3f042
pgm-casverheye <cas.verheye@student.arteveldehs.be>
2025-11-20 14:54:52 +0100

    set-up PWA

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+console.log('SW: Script loaded and parsed')
+
+self.addEventListener('install', (event) => {
+  console.log('SW: Install event fired!')
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  console.log('SW: Activate event fired!')
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', (event) => {
+  console.log('SW: Fetch event for', event.request.url)
+  event.respondWith(fetch(event.request))
+})

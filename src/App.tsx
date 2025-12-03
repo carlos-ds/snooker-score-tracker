99974b60f2bf1dd626597d6fcdd0d77868680a65
SegeKea26 <keano.segers@student.arteveldehs.be>
2025-12-03 14:04:54 +0100

    added test toggler

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Button from "./components/Button/Button"
+import Toggler from "./components/Toggler/Toggler"
 
 function App() {
   return (
     <div>
       <p>Hello world!</p>
+
+      <Toggler onChange={() => document.body.classList.toggle('dark')} />
     
       <Button value="boo" label="Click me" />
       <Button value="foo" label="Press me"/>

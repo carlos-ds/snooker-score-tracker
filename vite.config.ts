576f246b3c367164e1378b74d165b08ec2cd4954
pgm-casverheye <cas.verheye@student.arteveldehs.be>
2026-01-16 10:51:22 +0100

    feat: offline support

--- vite.config.ts
+++ vite.config.ts
@@ -24,4 +24,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    manifest: true, // Generate .vite/manifest.json for SW precache
+  },
 });
